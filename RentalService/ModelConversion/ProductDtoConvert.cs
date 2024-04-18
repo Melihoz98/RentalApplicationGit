f7d9c045035772c5b84539c1c351e51f49ba128c
Ahmad <10478728@ucn.dk>
2024-04-18 10:12:19 +0200

    ProductDtoConvert create and Product update

---
+++ RentalService/ModelConversion/ProductDtoConvert.cs
@@ -0,0 +1,50 @@
+﻿using RentalService.Models;
+using RentalService.DTO;
+
+
+namespace RentalService.ModelConversion
+{
+    public class ProductDtoConvert
+    {
+        // Convert from Product collection to ProductDto collection
+        public static List<ProductDto?>? FromProductCollection(List<Product> inProducts)
+        {
+            List<ProductDto?>? productDtoList = null;
+            if (inProducts != null)
+            {
+                productDtoList = new List<ProductDto?>();
+                foreach (Product product in inProducts)
+                {
+                    if (product != null)
+                    {
+                        ProductDto? dto = FromProduct(product);
+                        productDtoList.Add(dto);
+                    }
+                }
+            }
+            return productDtoList;
+        }
+
+        // Convert from Product to ProductDto
+        public static ProductDto? FromProduct(Product product)
+        {
+            ProductDto? productDto = null;
+            if (product != null)
+            {
+                productDto = new ProductDto(product.ProductName, product.Description, product.HourlyPrice, product.Inventory, product.CategoryID);
+            }
+            return productDto;
+        }
+
+        // Convert from ProductDto to Product
+        public static Product? ToProduct(ProductDto inDto)
+        {
+            Product? aProduct = null;
+            if (inDto != null)
+            {
+                aProduct = new Product(inDto.ProductName, inDto.Description, inDto.HourlyPrice, inDto.Inventory, inDto.CategoryID);
+            }
+            return aProduct;
+        }
+    }
+}
--- RentalService/Models/Product.cs
+++ RentalService/Models/Product.cs
@@ -2,12 +2,35 @@
 {
     public class Product
     {
+        public Product(){}
+
+        public Product(int productID, string? productName, string? description, decimal? hourlyPrice, int? inventory, int? categoryID)
+        {
+            ProductID = productID;
+            ProductName = productName;
+            Description = description;
+            HourlyPrice = hourlyPrice;
+            Inventory = inventory;
+            CategoryID = categoryID;
+        }
+
+        public Product(string? productName, string? description, decimal? hourlyPrice, int? inventory, int? categoryID)
+        {
+            ProductName = productName;
+            Description = description;
+            HourlyPrice = hourlyPrice;
+            Inventory = inventory;
+            CategoryID = categoryID;
+        }
+
+
+
         public int ProductID { get; set; }
-        public string ProductName { get; set; }
-        public string Description { get; set; }
-        public decimal HourlyPrice { get; set; }
-        public int Inventory { get; set; }
-        public int CategoryID { get; set; }
+        public string? ProductName { get; set; }
+        public string? Description { get; set; }
+        public decimal? HourlyPrice { get; set; }
+        public int? Inventory { get; set; }
+        public int? CategoryID { get; set; }
     }
 
 }
