866c4ee2fead6797cb8645ef715a7f101039888e
Melih Øzata <10487163@ucn.dk>
2024-04-23 09:56:24 +0200

    MVC application added with AUTH

---
+++ RentAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
