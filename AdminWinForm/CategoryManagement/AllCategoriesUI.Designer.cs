8c138fff2c89f7078c91d59885e8b79b5dfd1a2b
Ahmad <10478728@ucn.dk>
2024-04-29 09:38:11 +0200

    CategoryManagement merge

---
+++ AdminWinForm/CategoryManagement/AllCategoriesUI.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace AdminWinForm.CategoryManagement
+{
+    partial class AllCategoriesUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
+            dataGridView1.Location = new Point(1, 1);
+            dataGridView1.Margin = new Padding(2);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(797, 183);
+            dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(47, 400);
+            button1.Margin = new Padding(2);
+            button1.Name = "button1";
+            button1.Size = new Size(175, 27);
+            button1.TabIndex = 2;
+            button1.Text = "Add Category";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(296, 400);
+            button2.Margin = new Padding(2);
+            button2.Name = "button2";
+            button2.Size = new Size(175, 27);
+            button2.TabIndex = 3;
+            button2.Text = "Update Category";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(551, 400);
+            button3.Margin = new Padding(2);
+            button3.Name = "button3";
+            button3.Size = new Size(175, 27);
+            button3.TabIndex = 4;
+            button3.Text = "Delete Category";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "CategoryID";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            Column1.Width = 250;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Category Name";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.ReadOnly = true;
+            Column2.Width = 250;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "ImagePath";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            Column3.Width = 250;
+            // 
+            // AllCategoriesUI
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "AllCategoriesUI";
+            Text = "AllCategoriesUI";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+    }
+}
---
+++ AdminWinForm/CategoryManagement/AllCategoriesUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminWinForm.CategoryManagement
+{
+    public partial class AllCategoriesUI : Form
+    {
+        public AllCategoriesUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
