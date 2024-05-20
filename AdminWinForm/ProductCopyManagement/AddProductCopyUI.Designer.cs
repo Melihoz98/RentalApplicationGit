4d1aa3ca65d9b3940b3af9d06932b972c16cab12
MarwanAlmzayek <128796844+MarwanAlmzayek@users.noreply.github.com>
2024-05-20 15:25:34 +0200

    Product Dropdowan

--- AdminWinForm/ProductCopyManagement/AddProductCopyUI.Designer.cs
+++ AdminWinForm/ProductCopyManagement/AddProductCopyUI.Designer.cs
@@ -30,17 +30,18 @@
             label2 = new Label();
             label1 = new Label();
             textBox3 = new TextBox();
-            textBox2 = new TextBox();
             textBox1 = new TextBox();
             productName = new Label();
+            comboBox1 = new ComboBox();
             SuspendLayout();
             // 
             // button2
             // 
             button2.BackColor = SystemColors.ActiveCaption;
-            button2.Location = new Point(334, 373);
+            button2.Location = new Point(267, 298);
+            button2.Margin = new Padding(2, 2, 2, 2);
             button2.Name = "button2";
-            button2.Size = new Size(152, 34);
+            button2.Size = new Size(122, 27);
             button2.TabIndex = 23;
             button2.Text = "Save";
             button2.UseVisualStyleBackColor = false;
@@ -49,9 +50,10 @@
             // button1
             // 
             button1.BackColor = SystemColors.ActiveCaption;
-            button1.Location = new Point(79, 373);
+            button1.Location = new Point(63, 298);
+            button1.Margin = new Padding(2, 2, 2, 2);
             button1.Name = "button1";
-            button1.Size = new Size(152, 34);
+            button1.Size = new Size(122, 27);
             button1.TabIndex = 22;
             button1.Text = "Canncel";
             button1.UseVisualStyleBackColor = false;
@@ -60,72 +62,82 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(79, 274);
+            label4.Location = new Point(63, 219);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(0, 25);
+            label4.Size = new Size(0, 20);
             label4.TabIndex = 21;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(79, 216);
+            label3.Location = new Point(63, 173);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(0, 25);
+            label3.Size = new Size(0, 20);
             label3.TabIndex = 20;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(79, 159);
+            label2.Location = new Point(63, 127);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(101, 25);
+            label2.Size = new Size(83, 20);
             label2.TabIndex = 19;
             label2.Text = "Image Path";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(79, 103);
+            label1.Location = new Point(63, 82);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(92, 25);
+            label1.Size = new Size(75, 20);
             label1.TabIndex = 18;
             label1.Text = "ProductID";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(238, 153);
+            textBox3.Location = new Point(190, 122);
+            textBox3.Margin = new Padding(2, 2, 2, 2);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(248, 31);
+            textBox3.Size = new Size(199, 27);
             textBox3.TabIndex = 15;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(238, 97);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(248, 31);
-            textBox2.TabIndex = 14;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(238, 40);
+            textBox1.Location = new Point(190, 32);
+            textBox1.Margin = new Padding(2, 2, 2, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(248, 31);
+            textBox1.Size = new Size(199, 27);
             textBox1.TabIndex = 13;
             // 
             // productName
             // 
             productName.AutoSize = true;
-            productName.Location = new Point(79, 40);
+            productName.Location = new Point(63, 32);
+            productName.Margin = new Padding(2, 0, 2, 0);
             productName.Name = "productName";
-            productName.Size = new Size(124, 25);
+            productName.Size = new Size(104, 20);
             productName.TabIndex = 12;
             productName.Text = "Serial Number";
             // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(190, 79);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(199, 28);
+            comboBox1.TabIndex = 24;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
             // AddProductCopyUI
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(598, 450);
+            ClientSize = new Size(478, 360);
+            Controls.Add(comboBox1);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(label4);
@@ -133,9 +145,9 @@
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(textBox3);
-            Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(productName);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "AddProductCopyUI";
             Text = "AddProductCopyUI";
             ResumeLayout(false);
@@ -151,8 +163,8 @@
         private Label label2;
         private Label label1;
         private TextBox textBox3;
-        private TextBox textBox2;
         private TextBox textBox1;
         private Label productName;
+        private ComboBox comboBox1;
     }
 }
